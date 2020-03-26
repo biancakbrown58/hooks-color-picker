f7e808f72924da2efa17724dadc2be3d7040be3c
Bianca Brown <biancakbrown58@gmail.com>
2020-03-26 12:23:36 -0400

    explorer mode

--- src/App.js
+++ src/App.js
@@ -1,8 +1,70 @@
-import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import React, { useState } from 'react'
 
-const App = () => {
-  return <HelloWorld />
+const App = (props) => {
+  const [hue, setHue] = useState(Math.round(Math.random() * 360))
+  const [saturation, setSaturation] = useState(Math.round(Math.random() * 100))
+  const [light, setLight] = useState(Math.round(Math.random() * 100))
+  const [random, setRandom] = useState(Math.round(Math.random() * 256))
+
+  const randomHue = (e) => {
+    const hue = e.target.value
+    setHue(hue)
+  }
+  const randomSaturation = (e) => {
+    const saturation = e.target.value
+    setSaturation(saturation)
+  }
+  const randomLight = (e) => {
+    const light = e.target.value
+    setLight(light)
+  }
+  const newColor = `hsl(${hue}, ${saturation}%, ${light}%)`
+  console.log(newColor)
+
+  const randomColor = (e) => {
+    console.log('button clicked')
+    setRandom(random)
+  }
+
+  return (
+    <>
+      <section class="slidecontainer" style={{ backgroundColor: newColor }}>
+        <ul>
+          <li class="hue">
+            <input
+              type="range"
+              min="1"
+              max="360"
+              onChange={randomHue}
+              value={hue}
+              class="slider"
+            />
+          </li>
+
+          <li class="saturation">
+            <input
+              type="range"
+              min="1"
+              max="100"
+              onChange={randomSaturation}
+              value={saturation}
+              class="slider"
+            />
+          </li>
+          <li class="light">
+            <input
+              type="range"
+              min="1"
+              max="100"
+              onChange={randomLight}
+              value={light}
+              class="slider"
+            />
+          </li>
+          <button onClick={randomColor}>Random Color</button>
+        </ul>
+      </section>
+    </>
+  )
 }
-
 export default App
